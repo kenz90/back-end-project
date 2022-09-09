77089dc6d5cc61d7d00b89645317bb8ba34094de
Fudayl  Diedrick <fudayldiedrick@icloud.com>
2022-09-09 14:47:06 +0100

    user .catch added error

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,5 +3,5 @@ const { selectAllUsers } = require("../models/users.models");
 exports.getUsers = (req, res) => {
  selectAllUsers().then((users) => {
 res.status(200).send({ users });
-  }).catch();
+  }).catch((err));
 };
